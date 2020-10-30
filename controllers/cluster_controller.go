6fa0446caa335d2661603c5c3935e564945f4e44
Sunny <darkowlzz@protonmail.com>
2020-10-31 04:12:56 +0530

    cluster: Initialize status with available condition

--- controllers/cluster_controller.go
+++ controllers/cluster_controller.go
@@ -20,6 +20,8 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	conditions "github.com/openshift/custom-resource-status/conditions/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -38,10 +40,35 @@ type ClusterReconciler struct {
 // +kubebuilder:rbac:groups=darkowlzz.space,resources=clusters/status,verbs=get;update;patch
 
 func (r *ClusterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("cluster", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("cluster", req.NamespacedName)
 
 	// your logic here
+	log.Info("Cluster reconciling...")
+
+	var cluster darkowlzzspacev1.Cluster
+	if err := r.Get(ctx, req.NamespacedName, &cluster); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	init := true
+	if conditions.IsStatusConditionTrue(cluster.Status.Conditions, conditions.ConditionAvailable) {
+		init = false
+	}
+
+	// Initialize the cluster if uninitialized.
+	if init {
+		conditions.SetStatusCondition(&cluster.Status.Conditions, conditions.Condition{
+			Type:    conditions.ConditionAvailable,
+			Status:  corev1.ConditionTrue,
+			Reason:  "Initialized",
+			Message: "Initialized",
+		})
+		if err := r.Status().Update(ctx, &cluster); err != nil {
+			return ctrl.Result{Requeue: true}, err
+		}
+		log.Info("Cluster initialised")
+	}
 
 	return ctrl.Result{}, nil
 }
